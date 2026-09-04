728b26c7db5d73d16e60580f87b2d5493706f022
aman0069 <aachhikara007@gmail.com>
2026-09-05 01:29:09 +0530

    Serve frontend from Express server

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { prisma } from './db'
 
 const app = express()
 const port = Number(process.env.PORT ?? 3001)
+const currentDirectory = path.dirname(fileURLToPath(import.meta.url))
+const staticDirectory = path.resolve(currentDirectory, '../dist')
 
 app.use(cors())
 app.use(express.json())
@@ -24,6 +28,12 @@ app.get('/api/batches', async (_req, res) => {
   })
 
   res.json(batches)
+})
+
+app.use(express.static(staticDirectory))
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(staticDirectory, 'index.html'))
 })
 
 app.listen(port, () => {
